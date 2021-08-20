1e69649f9a9ef5450a0322b55b06e440ba77da95
bstmrc <mbasta.1402@gmail.com>
2021-08-19 21:17:00 -0300

    fix conventions errors

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python
+
+"""
+this module execute main program
+"""
 
 # MAIN
 import modulo as m
 
 def main():
-    pass
+    """contents all calls fuctions from modulo and user interact instances"""
 
 
 if __name__ == '__main__':
